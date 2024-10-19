e71ca58cfa498b22ecafbba4b928d09938d43159
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-10-19 13:16:26 -0400

    Update MoreAboutUs.tsx

--- components/MoreAboutUs.tsx
+++ components/MoreAboutUs.tsx
@@ -8,36 +8,66 @@ const MoreAboutUs = () => {
             bg-dark-white
             text-black
             flex
+            flex-wrap
             items-center
             justify-center
         ">
             <ul className="
                 grid
                 grid-cols-1
-                md:grid-cols-2
+                lg:grid-cols-2
                 gap-20
-                p-12
+                m-12
             ">
                 <li className="
-                    h-[500px]
+                    h-3/4
                     w-full
-                    bg-black
-                    text-white
+                    text-black
 
                 ">
-                    <div className="">
-                        ITEMS1
+                    <div className="
+                        box 
+                        border-2 #Remove this
+                        w-full
+                        h-[400px]
+                        align-top
+                        rounded-2xl
+                    ">
+                        <div className="
+                            w-[85%]
+                            h-[85%]
+                            bg-white
+                            border-black
+                            rounded-2xl
+                            mb-10
+                            mr-10
+                            -z-10              
+                        ">
+                            ITEMS1
+                        </div>
+                        <div className="
+                            -mt-44
+                            ml-[35%]
+                            box 
+                            border-2
+                            rounded-2xl
+                            h-[50%]
+                        ">
+                            ITEMS2
+                        </div>
                     </div>
                 </li>
                 <li className="
-                    h-[450px]
+                    h-[400px]
                     w-[500px]
-                    pt-16
+                    pt-4
                     pl-8
                 ">
                     <div className="
+                        hidden
+                        lg:flex
                         h-[100%]
-                        w-full                    
+                        w-[90%]
                         bg-black
                         text-white
                     ">
